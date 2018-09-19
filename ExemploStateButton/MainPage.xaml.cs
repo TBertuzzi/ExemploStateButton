4b7ced1ff4b3d796e610fe2dc0eacbc646a3842c
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-09-19 15:00:34 -0300

    :tada: subindo o código

---
+++ ExemploStateButton/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ExemploStateButton
+{
+    public partial class MainPage : ContentPage
+    {
+        private ICommand _selectedCommand;
+
+        public ICommand SelectCommand => _selectedCommand ?? (_selectedCommand = new Command((selectedItem) => lblbotao.Text = $"O botão {selectedItem.ToString()} foi apertado"));
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+            btnImagemAtiva.InnerPadding = new Thickness(10, 0);
+
+        }
+    }
+}
